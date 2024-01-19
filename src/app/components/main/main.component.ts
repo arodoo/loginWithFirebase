4ad8ff42e262d61812c3865a0fe885780c9c83e5
arodoo <haro.em@hotmail.com>
2024-01-18 20:57:17 -0600

    Update main component HTML and TypeScript files

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthGoogleService } from '../../services/auth-google.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
+  constructor(
+    private authGoogleService: AuthGoogleService,
+    private router: Router
+  ) { }
+
+  user: any;
+
+  ngOnInit() {
+    this.user = this.authGoogleService.getProfile();
+  }
+
+  showData() {
+    const data = JSON.stringify(this.authGoogleService.getProfile(), null, 2);
+    console.log(data);
+  }
+
+  logOut() {
+    this.authGoogleService.logout();
+    this.router.navigate(['login']);
+  }
 }
